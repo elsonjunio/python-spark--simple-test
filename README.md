81214a51657a046d35a513c69f2b70e14f7f6d94
elsonjunio <elsonjunio@gmail.com>
2021-08-24 21:05:31 -0300

    start commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # pyspark_test
+
+Create a new venv:
+
+```
+$ python3 -m venv venv
+$ source venv/bin/activate
+```
+
+Download jars
+
+```
+$ mkdir jars
+$ cd jars
+$ wget https://repo1.maven.org/maven2/org/mongodb/spark/mongo-spark-connector_2.12/2.4.0/mongo-spark-connector_2.12-2.4.0.jar
+$ wget https://repo1.maven.org/maven2/org/mongodb/mongo-java-driver/3.11.2/mongo-java-driver-3.11.2.jar
+```
+
+See more in https://docs.mongodb.com/spark-connector/current/python-api/
---
+++ main.py
@@ -0,0 +1,32 @@
+
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+working_directory = 'jars/*'
+
+my_spark = SparkSession \
+    .builder \
+    .appName("myApp") \
+    .config("spark.mongodb.input.uri=mongodb://127.0.0.1/test.myCollection") \
+    .config("spark.mongodb.output.uri=mongodb://127.0.0.1/test.myCollection") \
+    .config('spark.driver.extraClassPath', working_directory) \
+    .getOrCreate()
+
+people = my_spark.createDataFrame([("JULIA", 50), ("Gandalf", 1000), ("Thorin", 195), ("Balin", 178), ("Kili", 77),
+                            ("Dwalin", 169), ("Oin", 167), ("Gloin", 158), ("Fili", 82), ("Bombur", 22)], ["name", "age"])
+
+
+people.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").save()
+
+df = my_spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
+df.select('*').where(col("name") == "JULIA").show()
+
+
+df.filter(df['age'] >= 10).show()
+
+
+df.createOrReplaceTempView("temp")
+some_registers = my_spark.sql("SELECT name, age FROM temp WHERE name LIKE '%h%'")
+some_registers.show()
